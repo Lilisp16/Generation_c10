cd0187d478744c5e86b7aeb38ff969ea8ccbb5d8
Lilisp16 <lijhosa@gmail.com>
2025-10-31 12:23:43 -0500

    Lista de Compras

--- TallerPropiedades/listaCompras.js
+++ TallerPropiedades/listaCompras.js
@@ -5,3 +5,26 @@ Gestionar una lista dinámica de productos.Tareas:
 3. Implementa el método mostrar() que use un for para listar los productos numerados (ej: "1. Manzanas").
 4. Muestra la lista completa en consola. */
 
+
+function Compras() {
+    this.productos = [];
+    this.agregar = function(producto){
+        this.productos.push(producto);
+    };
+    this.mostrar = function(){     //otro metodo del objeto 
+        for(let i = 0; i < this.productos.length; i++){   // i empieza en 0 y se incrementa hasta el último índice del array
+            console.log(`${i + 1}. ${this.productos[i]}`) //aqui saco la lista numerada
+                                                        //i + 1 -> número visible para la lista (empezando en 1) es decir empieza numero en 1 
+                                                        //this.productos[i] -> valor real del producto en esa posición del array. es decir 0..
+
+        }
+    };
+
+}
+
+const lista = new Compras();
+lista.agregar("PepaEpan");
+lista.agregar("Chontaduro");
+
+lista.mostrar();
+
